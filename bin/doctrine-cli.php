09f51dd21a0cfcba330d891623730a516a012e63
Yulij Andreevich Lesov <yalesov@gmail.com>
2016-07-06 12:56:42 +0800

    use 2 spaces indentation

--- bin/doctrine-cli.php
+++ bin/doctrine-cli.php
@@ -8,21 +8,21 @@ chdir(__DIR__);
 $previousDir = '.';
 
 while (!file_exists('config/application.config.yml')) {
-    $dir = dirname(getcwd());
+  $dir = dirname(getcwd());
 
-    if ($previousDir === $dir) {
-        throw new RuntimeException(
-            'Unable to locate "config/application.config.yml": ' .
-            'is DoctrineModule in a subdir of your application skeleton?'
-        );
-    }
+  if ($previousDir === $dir) {
+    throw new RuntimeException(
+      'Unable to locate "config/application.config.yml": ' .
+      'is DoctrineModule in a subdir of your application skeleton?'
+    );
+  }
 
-    $previousDir = $dir;
-    chdir($dir);
+  $previousDir = $dir;
+  chdir($dir);
 }
 
 if (!(@include_once __DIR__ . '/../vendor/autoload.php') && !(@include_once __DIR__ . '/../../../autoload.php')) {
-    throw new RuntimeException('Error: vendor/autoload.php could not be found. Did you run php composer.phar install?');
+  throw new RuntimeException('Error: vendor/autoload.php could not be found. Did you run php composer.phar install?');
 }
 
 $application = Application::init(Yaml::parse('config/application.config.yml'));
